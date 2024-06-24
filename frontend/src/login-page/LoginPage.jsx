1ccbcec40c680f1ae008c6812daa0db7dae1a876
reindeer002 <lsj000424@gmail.com>
2024-06-24 07:48:53 +0000

    ADD find-password view in LoginPage

--- frontend/src/login-page/LoginPage.jsx
+++ frontend/src/login-page/LoginPage.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -11,6 +11,7 @@ import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import { Modal } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Copyright from "../commons/Copyright";
 
@@ -19,12 +20,25 @@ import Copyright from "../commons/Copyright";
 const defaultTheme = createTheme();
 
 export default function LoginPage() {
+  const [isFinding, setIsFinding] = useState(false);
+
+  const handleFindClick = () => {
+    setIsFinding(true);
+  };
+
+  const handleClose = () => {
+    setIsFinding(false);
+  };
+
+  const handleSendEmail = () => {
+    setIsFinding(false);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
       email: data.get("email"),
-      password: data.get("password"),
     });
   };
 
@@ -86,13 +100,13 @@ export default function LoginPage() {
             </Button>
             <Grid container>
               <Grid item xs>
-                <Link href="#" variant="body2">
+                <Link href="#" variant="body2" onClick={handleFindClick}>
                   비밀번호 찾기
                 </Link>
               </Grid>
               <Grid item>
                 <Link href="#" variant="body2">
-                  {"회원가입"}
+                  회원가입
                 </Link>
               </Grid>
             </Grid>
@@ -106,6 +120,45 @@ export default function LoginPage() {
         >
           <Copyright />
         </Typography>
+        <Modal open={isFinding} onClose={handleClose}>
+          <Box
+            component="form"
+            onSubmit={handleSendEmail}
+            noValidate
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              bgcolor: "background.paper",
+              boxShadow: 24,
+              p: 4,
+            }}
+          >
+            <Typography variant="h6" component="h2">
+              비밀번호 찾기
+            </Typography>
+            <TextField
+              label="비밀번호를 받을 이메일"
+              id="email"
+              name="email"
+              margin="normal"
+              variant="outlined"
+              required
+              autoFocus
+              fullWidth
+              sx={{ mt: 2 }}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              보내기
+            </Button>
+          </Box>
+        </Modal>
       </Container>
     </ThemeProvider>
   );
