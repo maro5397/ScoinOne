c4009fa9bbc6ed74fa88ad35a6fb34fca27b8755
reindeer002 <lsj000424@gmail.com>
2024-08-14 00:08:38 +0900

    ADD matching process when create each Orders

--- backend/core/src/main/java/com/scoinone/core/service/impl/BuyOrderServiceImpl.java
+++ backend/core/src/main/java/com/scoinone/core/service/impl/BuyOrderServiceImpl.java
@@ -3,20 +3,27 @@ package com.scoinone.core.service.impl;
 import com.scoinone.core.entity.BuyOrder;
 import com.scoinone.core.repository.BuyOrderRepository;
 import com.scoinone.core.service.BuyOrderService;
-import jakarta.persistence.EntityNotFoundException;
+import com.scoinone.core.service.TradeService;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class BuyOrderServiceImpl implements BuyOrderService {
+
+    private final EntityManager entityManager;
 
     private final BuyOrderRepository buyOrderRepository;
 
+    private final TradeService tradeService;
+
     @Override
     public BuyOrder createBuyOrder(BuyOrder buyOrder) {
+        entityManager.persist(buyOrder);
+        tradeService.processBuyOrderTrade(buyOrder);
         return buyOrderRepository.save(buyOrder);
     }
 
