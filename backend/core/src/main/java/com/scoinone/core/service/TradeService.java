626ec1c87c7d3ac61312ba2a1e8d8ed8e709c2e5
reindeer002 <lsj000424@gmail.com>
2024-08-14 00:09:12 +0900

    ADD and IMPLE matching logic

--- backend/core/src/main/java/com/scoinone/core/service/TradeService.java
+++ backend/core/src/main/java/com/scoinone/core/service/TradeService.java
@@ -1,7 +1,10 @@
 package com.scoinone.core.service;
 
+import com.scoinone.core.entity.BuyOrder;
+import com.scoinone.core.entity.SellOrder;
 import com.scoinone.core.entity.Trade;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface TradeService {
@@ -9,5 +12,9 @@ public interface TradeService {
 
     Trade getTradeById(Long id);
 
-    Trade createTrade(Trade trade);
+    Trade createTrade(BuyOrder buyOrder, SellOrder sellOrder, BigDecimal tradeQuantity);
+
+    List<Trade> processBuyOrderTrade(BuyOrder buyOrder);
+
+    List<Trade> processSellOrderTrade(SellOrder sellOrder);
 }
