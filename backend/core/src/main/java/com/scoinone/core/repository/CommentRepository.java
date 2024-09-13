34c6a9834fd6740708e769573593721409e4e86b
reindeer002 <lsj000424@gmail.com>
2024-09-13 10:41:00 +0900

    refactor: List<> to Page<>

--- backend/core/src/main/java/com/scoinone/core/repository/CommentRepository.java
+++ backend/core/src/main/java/com/scoinone/core/repository/CommentRepository.java
@@ -1,6 +1,8 @@
 package com.scoinone.core.repository;
 
 import com.scoinone.core.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +11,5 @@ import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-    Optional<List<Comment>> findByPost_PostId(Long postId);
+    Optional<Page<Comment>> findByPost_PostId(Pageable pageable, Long postId);
 }
