0a860b2abeaaf3be05c9efaedec13481539ec813
reindeer002 <lsj000424@gmail.com>
2024-09-13 10:35:43 +0900

    refactor: mod List<> to Page<>

--- backend/core/src/main/java/com/scoinone/core/service/PostService.java
+++ backend/core/src/main/java/com/scoinone/core/service/PostService.java
@@ -1,12 +1,12 @@
 package com.scoinone.core.service;
 
+import com.scoinone.core.common.PostType;
 import com.scoinone.core.entity.Post;
 import org.springframework.data.domain.Page;
-
-import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface PostService {
-    Page<Post> getPosts(int page, int size);
+    Page<Post> getPosts(Pageable pageable, PostType postType);
 
     Post getPostById(Long id);
 
