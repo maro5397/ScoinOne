faad986a80e2ba26e5de628a93dc21530e60969b
reindeer002 <lsj000424@gmail.com>
2024-09-13 10:41:27 +0900

    refactor: Date to LocalDateTime

--- backend/core/src/main/java/com/scoinone/core/repository/NotificationRepository.java
+++ backend/core/src/main/java/com/scoinone/core/repository/NotificationRepository.java
@@ -4,11 +4,11 @@ import com.scoinone.core.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    Optional<List<Notification>> findByUser_UserIdAndCreatedAtAfter(Long userId, Date date);
+    Optional<List<Notification>> findByUser_UserIdAndCreatedAtAfter(Long userId, LocalDateTime date);
 }
