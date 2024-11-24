adc274be9b7f4d11504e5b7dee45b50dd8e8d459
reindeer002 <lsj000424@gmail.com>
2024-11-24 23:04:36 +0900

    refactor: change using jpql to native query

--- backend/core/src/main/java/com/scoinone/core/repository/NotificationRepository.java
+++ backend/core/src/main/java/com/scoinone/core/repository/NotificationRepository.java
@@ -3,14 +3,18 @@ package com.scoinone.core.repository;
 import com.scoinone.core.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    @Query("SELECT n " +
-            "FROM Notification n " +
-            "WHERE n.user.id = :userId AND n.createdAt >= CURRENT_TIMESTAMP - 30")
-    List<Notification> findByUserIdAndLast30Days(Long userId);
+    @Query(
+            value = "SELECT * " +
+            "FROM notifications n " +
+            "WHERE n.user_id = :userId AND n.created_at >= NOW() - INTERVAL 30 DAY",
+            nativeQuery = true
+    )
+    List<Notification> findByUserIdAndLast30Days(@Param("userId") Long userId);
 }
