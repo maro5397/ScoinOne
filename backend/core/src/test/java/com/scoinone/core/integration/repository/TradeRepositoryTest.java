88115dbcd6913bcf8361f3a24b6926a42563369e
reindeer002 <lsj000424@gmail.com>
2024-11-24 18:03:09 +0900

    test: add trade repository test

---
+++ backend/core/src/test/java/com/scoinone/core/integration/repository/TradeRepositoryTest.java
@@ -0,0 +1,157 @@
+package com.scoinone.core.integration.repository;
+
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import com.scoinone.core.CoreApplication;
+import com.scoinone.core.common.OrderStatus;
+import com.scoinone.core.entity.BuyOrder;
+import com.scoinone.core.entity.SellOrder;
+import com.scoinone.core.entity.Trade;
+import com.scoinone.core.entity.User;
+import com.scoinone.core.entity.VirtualAsset;
+import com.scoinone.core.repository.BuyOrderRepository;
+import com.scoinone.core.repository.SellOrderRepository;
+import com.scoinone.core.repository.TradeRepository;
+import com.scoinone.core.repository.UserRepository;
+import com.scoinone.core.repository.VirtualAssetRepository;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest(classes = CoreApplication.class)
+class TradeRepositoryTest {
+    private User buyer;
+    private User seller;
+
+    @Autowired
+    private TradeRepository tradeRepository;
+
+    @Autowired
+    private BuyOrderRepository buyOrderRepository;
+
+    @Autowired
+    private SellOrderRepository sellOrderRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private VirtualAssetRepository virtualAssetRepository;
+
+    @Container
+    static MySQLContainer<?> sqlContainer = new MySQLContainer<>(
+            "mysql:8.0.34"
+    );
+
+    @DynamicPropertySource
+    public static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.driver-class-name", sqlContainer::getDriverClassName);
+        registry.add("spring.datasource.url", sqlContainer::getJdbcUrl);
+        registry.add("spring.datasource.~username", sqlContainer::getUsername);
+        registry.add("spring.datasource.password", sqlContainer::getPassword);
+    }
+
+    @BeforeEach
+    void setUp() {
+        buyer = User.builder()
+                .username("buyerUser")
+                .email("buyer@example.com")
+                .password("securePassword")
+                .build();
+        userRepository.save(buyer);
+
+        seller = User.builder()
+                .username("sellerUser")
+                .email("seller@example.com")
+                .password("securePassword")
+                .build();
+        userRepository.save(seller);
+
+        VirtualAsset virtualAsset = VirtualAsset.builder()
+                .name("Bitcoin")
+                .symbol("BTC")
+                .description("A decentralized digital currency")
+                .build();
+        virtualAssetRepository.save(virtualAsset);
+
+        BuyOrder buyOrder = BuyOrder.builder()
+                .buyer(buyer)
+                .virtualAsset(virtualAsset)
+                .quantity(BigDecimal.valueOf(1))
+                .price(BigDecimal.valueOf(50000))
+                .status(OrderStatus.COMPLETED)
+                .build();
+        buyOrderRepository.save(buyOrder);
+
+        SellOrder sellOrder = SellOrder.builder()
+                .seller(seller)
+                .virtualAsset(virtualAsset)
+                .quantity(BigDecimal.valueOf(1))
+                .price(BigDecimal.valueOf(50000))
+                .status(OrderStatus.COMPLETED)
+                .build();
+        sellOrderRepository.save(sellOrder);
+
+        Trade trade = Trade.builder()
+                .buyOrder(buyOrder)
+                .sellOrder(sellOrder)
+                .virtualAsset(virtualAsset)
+                .quantity(BigDecimal.valueOf(1))
+                .price(BigDecimal.valueOf(50000))
+                .build();
+        tradeRepository.save(trade);
+    }
+
+    @AfterEach
+    void tearDown() {
+        tradeRepository.deleteAll();
+        buyOrderRepository.deleteAll();
+        sellOrderRepository.deleteAll();
+        userRepository.deleteAll();
+        virtualAssetRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("구매 주문의 구매자 ID로 체결된 거래 조회")
+    void testFindByBuyOrder_Buyer_Id() {
+        Long buyerId = buyer.getId();
+
+        Optional<List<Trade>> trades = tradeRepository.findByBuyOrder_Buyer_Id(buyerId);
+
+        assertSoftly(softly -> {
+            softly.assertThat(trades).isPresent();
+            trades.ifPresent(trade -> {
+                softly.assertThat(trade).hasSize(1);
+                softly.assertThat(trade.getFirst().getBuyOrder().getBuyer().getId()).isEqualTo(buyerId);
+            });
+        });
+    }
+
+    @Test
+    @DisplayName("판매 주문의 판매자 ID로 체결된 거래 조회")
+    void testFindBySellOrder_Seller_Id() {
+        Long sellerId = seller.getId();
+
+        Optional<List<Trade>> trades = tradeRepository.findBySellOrder_Seller_Id(sellerId);
+
+        assertSoftly(softly -> {
+            softly.assertThat(trades).isPresent();
+            trades.ifPresent(trade -> {
+                softly.assertThat(trade).hasSize(1);
+                softly.assertThat(trade.getFirst().getSellOrder().getSeller().getId()).isEqualTo(sellerId);
+            });
+        });
+    }
+}
