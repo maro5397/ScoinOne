a1d360bed077282a83515c896435447d4dd8b5d9
reindeer002 <lsj000424@gmail.com>
2024-11-26 17:50:01 +0900

    refactor: erase unnecessary field

--- backend/core/src/main/java/com/scoinone/core/dto/request/notification/CreateNotificationRequestDto.java
+++ backend/core/src/main/java/com/scoinone/core/dto/request/notification/CreateNotificationRequestDto.java
@@ -1,17 +1,11 @@
 package com.scoinone.core.dto.request.notification;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.scoinone.core.common.NotificationStatus;
-import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class CreateNotificationRequestDto {
-    private Long userId;
+    private String email;
     private String message;
-    private NotificationStatus status;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime expiresAt;
 }
