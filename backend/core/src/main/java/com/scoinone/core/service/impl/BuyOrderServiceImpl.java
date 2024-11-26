1469e91130f1a5428e9389f95ea07bf0406ef3c4
reindeer002 <lsj000424@gmail.com>
2024-11-26 16:10:22 +0900

    feat: implement buy order service crud functions

--- backend/core/src/main/java/com/scoinone/core/service/impl/BuyOrderServiceImpl.java
+++ backend/core/src/main/java/com/scoinone/core/service/impl/BuyOrderServiceImpl.java
@@ -1,10 +1,17 @@
 package com.scoinone.core.service.impl;
 
+import com.scoinone.core.common.OrderStatus;
 import com.scoinone.core.entity.BuyOrder;
+import com.scoinone.core.entity.User;
+import com.scoinone.core.entity.VirtualAsset;
 import com.scoinone.core.repository.BuyOrderRepository;
+import com.scoinone.core.repository.VirtualAssetRepository;
 import com.scoinone.core.service.BuyOrderService;
 import com.scoinone.core.service.TradeService;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityNotFoundException;
+import java.math.BigDecimal;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,17 +25,34 @@ public class BuyOrderServiceImpl implements BuyOrderService {
 
     private final BuyOrderRepository buyOrderRepository;
 
+    private final VirtualAssetRepository virtualAssetRepository;
+
     private final TradeService tradeService;
 
     @Override
-    public BuyOrder createBuyOrder(BuyOrder buyOrder) {
+    public BuyOrder createBuyOrder(Long assetId, BigDecimal quantity, BigDecimal price, User user) {
+        VirtualAsset virtualAsset = virtualAssetRepository.findById(assetId)
+                .orElseThrow(() -> new EntityNotFoundException("VirtualAsset not found with id: " + assetId));
+        BuyOrder buyOrder = BuyOrder.builder()
+                .buyer(user)
+                .price(price)
+                .quantity(quantity)
+                .status(OrderStatus.PENDING)
+                .virtualAsset(virtualAsset)
+                .build();
         entityManager.persist(buyOrder);
         tradeService.processBuyOrderTrade(buyOrder);
         return buyOrderRepository.save(buyOrder);
     }
 
     @Override
-    public void deleteBuyOrder(Long id) {
+    public List<BuyOrder> getBuyOrders(User user) {
+        return buyOrderRepository.findByBuyer_IdAndStatus(user.getId(), OrderStatus.PENDING);
+    }
+
+    @Override
+    public String deleteBuyOrder(Long id) {
         buyOrderRepository.deleteById(id);
+        return "BuyOrder deleted successfully";
     }
 }
