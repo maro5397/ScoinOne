6b2845572a3daaed649001f8a38e482448a6c58f
reindeer002 <lsj000424@gmail.com>
2024-11-26 16:20:58 +0900

    feat: implement sell order crud api

--- backend/core/src/main/java/com/scoinone/core/controller/SellOrderController.java
+++ backend/core/src/main/java/com/scoinone/core/controller/SellOrderController.java
@@ -1,4 +1,64 @@
 package com.scoinone.core.controller;
 
+import com.scoinone.core.auth.LoginUser;
+import com.scoinone.core.dto.common.DeleteResponseDto;
+import com.scoinone.core.dto.request.order.CreateSellOrderRequestDto;
+import com.scoinone.core.dto.response.order.CreateSellOrderResponseDto;
+import com.scoinone.core.dto.response.order.GetSellOrdersResponseDto;
+import com.scoinone.core.entity.SellOrder;
+import com.scoinone.core.entity.User;
+import com.scoinone.core.mapper.OrderMapper;
+import com.scoinone.core.service.SellOrderService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/sell")
+@RequiredArgsConstructor
 public class SellOrderController {
+    private final SellOrderService sellOrderService;
+
+    @PostMapping("/{virtualAssetId}")
+    public ResponseEntity<CreateSellOrderResponseDto> createSellOrder(
+            @PathVariable Long virtualAssetId,
+            @RequestBody CreateSellOrderRequestDto requestDto,
+            @LoginUser User user
+    ) {
+        SellOrder sellOrder = sellOrderService.createSellOrder(
+                virtualAssetId,
+                requestDto.getQuantity(),
+                requestDto.getPrice(),
+                user
+        );
+        return new ResponseEntity<>(
+                OrderMapper.INSTANCE.sellOrderToCreateSellOrderResponseDto(sellOrder),
+                HttpStatus.CREATED
+        );
+    }
+
+    @GetMapping
+    public ResponseEntity<GetSellOrdersResponseDto> getSellOrder(
+            @LoginUser User user
+    ) {
+        List<SellOrder> SellOrders = sellOrderService.getSellOrders(user);
+        return new ResponseEntity<>(OrderMapper.INSTANCE.listToGetSellOrderListResponseDto(SellOrders), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{orderId}")
+    public ResponseEntity<DeleteResponseDto> deleteSellOrder(
+            @PathVariable Long orderId
+    ) {
+        String result = sellOrderService.deleteSellOrder(orderId);
+        DeleteResponseDto deleteResponseDto = new DeleteResponseDto(result);
+        return new ResponseEntity<>(deleteResponseDto, HttpStatus.OK);
+    }
 }
