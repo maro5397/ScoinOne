0c01cc966268b031224ef0914f1ae04e1f1d0362
reindeer002 <lsj000424@gmail.com>
2024-11-28 04:02:43 +0900

    test: add integration test for auth controller

---
+++ backend/core/src/test/java/com/scoinone/core/integration/controller/AuthControllerTest.java
@@ -0,0 +1,51 @@
+package com.scoinone.core.integration.controller;
+
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import com.scoinone.core.config.TestContainerConfig;
+import com.scoinone.core.dto.request.auth.LoginRequestDto;
+import com.scoinone.core.dto.response.auth.LoginResponseDto;
+import com.scoinone.core.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import({TestContainerConfig.class})
+class AuthControllerTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @BeforeEach
+    void setUp() {
+        userService.createUser("test@example.com", "password", "testUser");
+    }
+
+    @Test
+    void signIn_shouldReturnJwtAndHttpStatusOk() {
+        LoginRequestDto requestDto = new LoginRequestDto();
+        requestDto.setEmail("test@example.com");
+        requestDto.setPassword("password");
+
+        ResponseEntity<LoginResponseDto> response = restTemplate.postForEntity(
+                "/api/auth/signin",
+                requestDto,
+                LoginResponseDto.class
+        );
+
+        assertSoftly(softly -> {
+            softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+            softly.assertThat(response.getHeaders().get("Authorization"))
+                    .anySatisfy(header -> softly.assertThat(header).startsWith("Bearer "));
+        });
+    }
+}
