b78e586586e23c192ea82938417b55d838137e4c
reindeer002 <lsj000424@gmail.com>
2024-11-28 02:31:16 +0900

    feat: add setting code for authority

---
+++ backend/core/src/main/java/com/scoinone/core/util/DataInitializer.java
@@ -0,0 +1,30 @@
+package com.scoinone.core.util;
+
+import com.scoinone.core.entity.Authority;
+import com.scoinone.core.repository.AuthorityRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements ApplicationRunner {
+
+    private final AuthorityRepository authorityRepository;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        Authority roleUser = Authority.builder()
+                .authorityName("ROLE_USER")
+                .build();
+        Authority roleAdmin = Authority.builder()
+                .authorityName("ROLE_ADMIN")
+                .build();
+        Authority roleManager = Authority.builder()
+                .authorityName("ROLE_MANAGER")
+                .build();
+        authorityRepository.saveAll(List.of(roleUser, roleAdmin, roleManager));
+    }
+}
