45e08689cc3e59f1e8564eecdc2f1643f28afc7b
reindeer002 <lsj000424@gmail.com>
2024-11-27 18:33:46 +0900

    refactor: separate service function

--- backend/core/src/main/java/com/scoinone/core/controller/AuthController.java
+++ backend/core/src/main/java/com/scoinone/core/controller/AuthController.java
@@ -1,18 +1,14 @@
 package com.scoinone.core.controller;
 
 import com.scoinone.core.auth.JwtFilter;
-import com.scoinone.core.auth.JwtTokenProvider;
 import com.scoinone.core.dto.request.auth.LoginRequestDto;
 import com.scoinone.core.dto.response.auth.LoginResponseDto;
+import com.scoinone.core.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,20 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
-    private final JwtTokenProvider jwtTokenProvider;
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDto> authorize(@Valid @RequestBody LoginRequestDto requestDto) {
-
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                requestDto.getEmail(),
-                requestDto.getPassword()
-        );
-
-        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwt = jwtTokenProvider.createToken(authentication);
+    public ResponseEntity<LoginResponseDto> signIn(@Valid @RequestBody LoginRequestDto requestDto) {
+        String jwt = authService.authenticate(requestDto.getEmail(), requestDto.getPassword());
 
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
