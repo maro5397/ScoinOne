e4bd5af9738bf6c269f0232d6a250073e79cf406
reindeer002 <lsj000424@gmail.com>
2024-11-29 21:07:12 +0900

    refactor: add order to load and run in application runner

--- backend/core/src/main/java/com/scoinone/core/util/DataInitializer.java
+++ backend/core/src/main/java/com/scoinone/core/util/DataInitializer.java
@@ -6,10 +6,12 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Order(1)
 public class DataInitializer implements ApplicationRunner {
 
     private final AuthorityRepository authorityRepository;
