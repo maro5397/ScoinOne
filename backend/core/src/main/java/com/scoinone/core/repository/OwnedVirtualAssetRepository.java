0899a90ae8ebc882761654501de72230c1f578ab
reindeer002 <lsj000424@gmail.com>
2024-11-30 02:05:43 +0900

    feat: add function that find owned virtual asset by user_id and virtual_asset_id

--- backend/core/src/main/java/com/scoinone/core/repository/OwnedVirtualAssetRepository.java
+++ backend/core/src/main/java/com/scoinone/core/repository/OwnedVirtualAssetRepository.java
@@ -1,6 +1,7 @@
 package com.scoinone.core.repository;
 
 import com.scoinone.core.entity.OwnedVirtualAsset;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,5 @@ import java.util.List;
 @Repository
 public interface OwnedVirtualAssetRepository extends JpaRepository<OwnedVirtualAsset, Long> {
     List<OwnedVirtualAsset> findByUser_Id(Long userId);
+    Optional<OwnedVirtualAsset> findByUser_IdAndVirtualAsset_Id(Long userId, Long assetId);
 }
