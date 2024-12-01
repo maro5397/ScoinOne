2fbc62b4b1858d4e002005f3e66574d13601f33a
reindeer002 <lsj000424@gmail.com>
2024-12-02 00:11:37 +0900

    refactor: erase logger

--- backend/core/src/main/java/com/scoinone/core/auth/JwtFilter.java
+++ backend/core/src/main/java/com/scoinone/core/auth/JwtFilter.java
@@ -7,8 +7,6 @@ import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
@@ -19,7 +17,6 @@ public class JwtFilter extends GenericFilterBean {
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String AUTHORIZATION_MODIFIER = "Bearer";
 
-    private final Logger logger = LoggerFactory.getLogger(JwtFilter.class);
     private final JwtTokenProvider tokenProvider;
 
     @Override
@@ -30,14 +27,10 @@ public class JwtFilter extends GenericFilterBean {
     ) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         String jwt = resolveToken(httpServletRequest);
-        String requestURI = httpServletRequest.getRequestURI();
 
         if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
             Authentication authentication = tokenProvider.getAuthentication(jwt);
             SecurityContextHolder.getContext().setAuthentication(authentication);
-            logger.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
-        } else {
-            logger.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
         }
         filterChain.doFilter(servletRequest, servletResponse);
     }
