cbb567e1595e5a7271df5a62eedc4bcec463097d
reindeer002 <lsj000424@gmail.com>
2024-12-01 19:55:57 +0900

    chore: annotate in method for extending features in future

--- backend/core/src/main/java/com/scoinone/core/service/UserService.java
+++ backend/core/src/main/java/com/scoinone/core/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import java.util.List;
 
 public interface UserService extends UserDetailsService {
+    // 추후 다른 사용자 정보 조회 시 사용
     User getUserByEmail(String email);
 
     User createUser(String email, String password, String username);
@@ -22,6 +23,7 @@ public interface UserService extends UserDetailsService {
 
     List<SellOrder> getSellOrderByUserId(Long userId);
 
+    // 추후 모든 주문에 대한 조회 기능 구현 시 사용
     List<Object> getOrderByUserId(Long userId);
 
     List<Trade> getTradeByUserId(Long userId);
