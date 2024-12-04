a43e1f260ad81472522bb55538bb48b1b46ccd76
seoxuni <seoxuni@kakao.com>
2024-12-04 13:32:49 +0900

    fix: delete useless annotations

--- frontend/src/landing-page/LandingPage.jsx
+++ frontend/src/landing-page/LandingPage.jsx
@@ -1,8 +1,6 @@
 import Introduction from "./components/Introduction";
 import Slider from "./components/Slider";
 import Announce from "./components/Announce";
-// import FAQ from "./components/FAQ";
-// import ArticleLayout from "./components/ArticleLayout";
 import BaseLayout from "../commons/BaseLayout";
 
 export default function LandingPage() {
