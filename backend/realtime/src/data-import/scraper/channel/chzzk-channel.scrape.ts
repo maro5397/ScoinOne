11671b22d6b5c99af223c52cfeedb2ee7470990f
reindeer002 <lsj000424@gmail.com>
2024-12-08 06:40:52 +0000

    feat: scrape follower at chzzk and soop live streaming platform

---
+++ backend/realtime/src/data-import/scraper/channel/chzzk-channel.scrape.ts
@@ -0,0 +1,21 @@
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ChzzkChannelScrape {
+  constructor(private readonly httpService: HttpService) {}
+
+  async getFollowerCount(streamerId: string) {
+    const url = `https://api.chzzk.naver.com/service/v1/channels/${streamerId}`;
+
+    try {
+      const response = await firstValueFrom(this.httpService.get(url));
+      const followerCount = response.data.content.followerCount;
+      return { followerCount };
+    } catch (error) {
+      console.error('Error fetching follower count:', error);
+      throw new Error('Failed to fetch follower count');
+    }
+  }
+}
---
+++ backend/realtime/src/data-import/scraper/channel/soop-channel.scrape.ts
@@ -0,0 +1,21 @@
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class SoopChannelScrape {
+  constructor(private readonly httpService: HttpService) {}
+
+  async getFollowerCount(streamerId: string) {
+    const url = `https://chapi.sooplive.co.kr/api/${streamerId}/station`;
+
+    try {
+      const response = await firstValueFrom(this.httpService.get(url));
+      const followerCount = response.data.station.upd.fan_cnt;
+      return { followerCount };
+    } catch (error) {
+      console.error('Error fetching follower count:', error);
+      throw new Error('Failed to fetch follower count');
+    }
+  }
+}
