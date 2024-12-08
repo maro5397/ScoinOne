90e03f22fde6772643ab3b2d9ed1f0a8ee0fe1f5
reindeer002 <lsj000424@gmail.com>
2024-12-08 06:13:31 +0000

    feat: add naver cafe scraper

---
+++ backend/realtime/src/data-import/scraper/fandom/naver-cafe.scrape.ts
@@ -0,0 +1,17 @@
+import { chromium } from 'playwright';
+
+export class NaverCafeScrape {
+  async getIntroduceData(cafeId: string) {
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
+    await page.goto(`https://cafe.naver.com/CafeProfileView.nhn?clubid=${cafeId}`);
+
+    const data1 = await page.locator('//*[@id="main-area"]/div/table/tbody/tr[14]/td/span[1]').textContent();
+    const data2 = await page.locator('//*[@id="main-area"]/div/table/tbody/tr[14]/td/span[2]').textContent();
+    const data3 = await page.locator('//*[@id="main-area"]/div/table/tbody/tr[14]/td/span[3]').textContent();
+
+    await browser.close();
+
+    return { data1, data2, data3 };
+  }
+}
