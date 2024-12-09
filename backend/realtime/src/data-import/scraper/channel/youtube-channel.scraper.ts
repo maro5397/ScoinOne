87f3fe5256bd85e2957021fa658fe097b79e8917
reindeer002 <lsj000424@gmail.com>
2024-12-09 17:49:09 +0000

    refactor: change query string id to forHandle

--- backend/realtime/src/data-import/scraper/channel/youtube-channel.scraper.ts
+++ backend/realtime/src/data-import/scraper/channel/youtube-channel.scraper.ts
@@ -5,25 +5,30 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class YoutubeChannelScraper {
-  private readonly API_KEY: string;
+  private readonly YOUTUBE_API_KEY: string;
 
   constructor(
     private readonly configService: ConfigService,
     private readonly httpService: HttpService,
   ) {
-    this.API_KEY = this.configService.get<string>('YOUTUBE_API_KEY');
+    this.YOUTUBE_API_KEY = this.configService.get<string>('YOUTUBE_API_KEY');
   }
 
   async getChannelStatistics(channelId: string) {
-    const url = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelId}&key=${this.API_KEY}`;
+    const url = `https://www.googleapis.com/youtube/v3/channels?part=statistics&forHandle=${channelId}&key=${this.YOUTUBE_API_KEY}`;
 
     try {
       const response = await firstValueFrom(this.httpService.get(url));
       const statistics = response.data.items[0].statistics;
-      return { subscriberCount: statistics.subscriberCount, viewCount: statistics.viewCount };
+      if (response.data.items.length > 1) {
+        throw new Error('Exploring duplicate users');
+      }
+      if (response.data.items.length === 0) {
+        throw new Error('There is no user');
+      }
+      return { subscriberCount: Number(statistics.subscriberCount), viewCount: Number(statistics.viewCount) };
     } catch (error) {
-      console.error('Error fetching channel stats:', error);
-      throw new Error('Failed to fetch channel stats');
+      throw new Error(error);
     }
   }
 }
