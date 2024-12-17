875ed15f77c32727579a8b1d65e3113ac5b7268c
reindeer002 <lsj000424@gmail.com>
2024-12-17 08:31:18 +0000

    refactor: use chzzk library and erase DI component

--- backend/realtime/src/data-import/test/integration/scraper/live/chzzk/chzzk-live-chat.scraper.simulation.ts
+++ backend/realtime/src/data-import/test/integration/scraper/live/chzzk/chzzk-live-chat.scraper.simulation.ts
@@ -1,16 +1,12 @@
-import { HttpService } from '@nestjs/axios';
 import { ChzzkLiveChatScraper } from '../../../../../scraper/live/chzzk/chzzk-live-chat.scraper';
 
 (async function () {
-  const httpService = new HttpService();
-
-  const scraper = new ChzzkLiveChatScraper(httpService);
+  const scraper = new ChzzkLiveChatScraper();
 
   try {
-    const streamerId = 'a6c4ddb09cdb160478996007bff35296';
-    const liveChatInfo = await scraper.getLiveChats(streamerId);
-    console.log('Live Chat Info:', liveChatInfo);
+    const streamerId = '75cbf189b3bb8f9f687d2aca0d0a382b';
+    await scraper.getLiveChats(streamerId);
   } catch (error) {
-    console.error('Error fetching live chat info:', error.message);
+    console.error(error.message);
   }
 })();
--- backend/realtime/src/data-import/test/integration/scraper/live/chzzk/chzzk-live-viewer.scraper.spec.ts
+++ backend/realtime/src/data-import/test/integration/scraper/live/chzzk/chzzk-live-viewer.scraper.spec.ts
@@ -1,28 +1,12 @@
-import { TestingModule, Test } from '@nestjs/testing';
-import { HttpModule } from '@nestjs/axios';
 import { ChzzkLiveViewerScraper } from '../../../../../scraper/live/chzzk/chzzk-live-viewer.scraper';
 
-const HANDONGSUK_ID = '75cbf189b3bb8f9f687d2aca0d0a382b';
+const STREAMER_ID = '75cbf189b3bb8f9f687d2aca0d0a382b';
 
 describe('ChzzkChannelScraperTest', () => {
-  let scraper: ChzzkLiveViewerScraper;
-
-  beforeAll(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        HttpModule.register({
-          timeout: 5000,
-          maxRedirects: 5,
-        }),
-      ],
-      providers: [ChzzkLiveViewerScraper],
-    }).compile();
-
-    scraper = module.get<ChzzkLiveViewerScraper>(ChzzkLiveViewerScraper);
-  });
+  const scraper: ChzzkLiveViewerScraper = new ChzzkLiveViewerScraper();
 
   it('should return follower count', async () => {
-    const result = await scraper.getViewerCount(HANDONGSUK_ID);
+    const result = await scraper.getViewerCount(STREAMER_ID);
     expect(result.live).toBeDefined();
     expect(typeof result.live).toBe('boolean');
     if (result.live) {
