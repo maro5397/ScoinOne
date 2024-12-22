14bff5714c0d9717ee7bde2a82b5c25236b8872c
reindeer002 <lsj000424@gmail.com>
2024-12-22 08:20:13 +0000

    refactor: add viewerCount check that be greater than or equal to 0

--- backend/realtime/src/data-import/test/integration/scraper/live/chzzk/chzzk-live-viewer.scraper.spec.ts
+++ backend/realtime/src/data-import/test/integration/scraper/live/chzzk/chzzk-live-viewer.scraper.spec.ts
@@ -1,15 +1,17 @@
 import { ChzzkLiveViewerScraper } from '../../../../../scraper/live/chzzk/chzzk-live-viewer.scraper';
 
-const STREAMER_ID = '75cbf189b3bb8f9f687d2aca0d0a382b';
+const STREAMER_ID = 'a86031fdfeb1b223ad1172cf6ba4b94e';
 
-describe('ChzzkChannelScraperTest', () => {
+describe('ChzzkLiveViewerScraperTest', () => {
   const scraper: ChzzkLiveViewerScraper = new ChzzkLiveViewerScraper();
 
-  it('should return follower count', async () => {
+  it('should return viewer count', async () => {
     const result = await scraper.getViewerCount(STREAMER_ID);
     expect(result.live).toBeDefined();
     expect(typeof result.live).toBe('boolean');
     if (result.live) {
+      console.log(result.viewerCount);
+      expect(result.viewerCount).toBeGreaterThanOrEqual(0);
       expect(result.viewerCount).toBeDefined();
       expect(typeof result.viewerCount).toBe('number');
     }
