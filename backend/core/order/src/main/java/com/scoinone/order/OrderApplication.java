e403241f44c8d674296e63c4a9f1d1a8ec2535c6
reindeer002 <lsj000424@gmail.com>
2025-01-15 22:31:30 +0900

    feat: add jpa annotation for jpa and auditing created, updated

--- backend/core/order/src/main/java/com/scoinone/order/OrderApplication.java
+++ backend/core/order/src/main/java/com/scoinone/order/OrderApplication.java
@@ -2,8 +2,12 @@ package com.scoinone.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableJpaAuditing
 public class OrderApplication {
 
 	public static void main(String[] args) {
