bb8a32c9ff36f6fa3a7d7c397235a92a98fa6628
reindeer002 <lsj000424@gmail.com>
2025-01-16 03:52:35 +0900

    feat: add entity from single service architecture

---
+++ backend/core/order/src/main/java/com/scoinone/order/entity/base/CreatableEntity.java
@@ -0,0 +1,23 @@
+package com.scoinone.order.entity.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@SuperBuilder
+public abstract class CreatableEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+}
