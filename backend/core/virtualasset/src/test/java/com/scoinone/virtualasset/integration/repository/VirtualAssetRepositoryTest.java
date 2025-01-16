f5fa1812d884ef95fe04598955658c7a75447049
reindeer002 <lsj000424@gmail.com>
2025-01-17 02:16:32 +0900

    test: add integration test for controller and repository

---
+++ backend/core/virtualasset/src/test/java/com/scoinone/virtualasset/integration/repository/VirtualAssetRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.scoinone.virtualasset.integration.repository;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class VirtualAssetRepositoryTest {
+
+}
