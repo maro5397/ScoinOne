2377414bb031664d6f4c069660ed22c0cae1fa33
reindeer002 <lsj000424@gmail.com>
2025-01-17 00:23:10 +0900

    feat: add user domain entity

---
+++ backend/core/user/src/main/java/com/scoinone/user/entity/UserEntity.java
@@ -0,0 +1,67 @@
+package com.scoinone.user.entity;
+
+import com.scoinone.user.entity.base.UpdatableEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class UserEntity extends UpdatableEntity implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Getter
+    private String id;
+
+    @Setter
+    private String username;
+
+    @Getter
+    private String email;
+
+    @Setter
+    @Getter
+    private String password;
+
+    @Setter
+    @Getter
+    private LocalDateTime lastLogin;
+
+    @Setter
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Set<UserAuthorityEntity> userAuthorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return userAuthorities.stream().map(UserAuthorityEntity::getAuthority).collect(Collectors.toSet());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    public String getCustomUsername() {
+        return username;
+    }
+}
