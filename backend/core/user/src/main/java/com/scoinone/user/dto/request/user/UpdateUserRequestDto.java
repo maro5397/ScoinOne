fbcc24eaeed8f6be3de2fac828fa8ca6399d7726
reindeer002 <lsj000424@gmail.com>
2025-01-17 00:22:56 +0900

    feat: add user, notification dto

---
+++ backend/core/user/src/main/java/com/scoinone/user/dto/request/user/UpdateUserRequestDto.java
@@ -0,0 +1,10 @@
+package com.scoinone.user.dto.request.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateUserRequestDto {
+    private String username;
+}
---
+++ backend/core/user/src/main/java/com/scoinone/user/dto/response/user/GetUserResponseDto.java
@@ -0,0 +1,18 @@
+package com.scoinone.user.dto.response.user;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GetUserResponseDto {
+    private String userId;
+    private String username;
+    private String email;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updatedAt;
+}
