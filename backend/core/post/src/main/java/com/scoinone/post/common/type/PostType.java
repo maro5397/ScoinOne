04cc9f447fa0cd9cfcfb904a9663c487d2f17df9
reindeer002 <lsj000424@gmail.com>
2025-01-19 04:43:31 +0900

    refactor: change value into lower case and add from function

--- backend/core/post/src/main/java/com/scoinone/post/common/type/PostType.java
+++ backend/core/post/src/main/java/com/scoinone/post/common/type/PostType.java
@@ -4,12 +4,21 @@ import lombok.Getter;
 
 @Getter
 public enum PostType {
-    QNA("QUESTION"),
-    ANNOUNCEMENT("ANNOUNCEMENT");
+    QNA("question"),
+    ANNOUNCEMENT("announcement"),;
 
     private final String value;
 
     PostType(String value) {
         this.value = value;
+    }
+
+    public static PostType from(String value) {
+        for (PostType type : PostType.values()) {
+            if (type.value.equals(value)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Invalid PostType value: " + value);
     }
 }
