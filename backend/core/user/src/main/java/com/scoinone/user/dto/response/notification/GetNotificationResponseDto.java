6f0be3241c2acb7d4c89b1e981367e1eb8b68275
reindeer002 <lsj000424@gmail.com>
2025-01-20 02:44:13 +0900

    refactor: erase expiresAt attribute

--- backend/core/user/src/main/java/com/scoinone/user/dto/response/notification/GetNotificationResponseDto.java
+++ backend/core/user/src/main/java/com/scoinone/user/dto/response/notification/GetNotificationResponseDto.java
@@ -14,6 +14,4 @@ public class GetNotificationResponseDto {
     private String status;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime expiresAt;
 }
