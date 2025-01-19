1a01914555545f523fdf1e371f81f016d0911032
reindeer002 <lsj000424@gmail.com>
2025-01-20 03:10:43 +0900

    feat: add two apis
    - add get notifications from last 30 day
    - add sse api to get notification live stream

--- backend/core/user/src/main/java/com/scoinone/user/controller/NotificationController.java
+++ backend/core/user/src/main/java/com/scoinone/user/controller/NotificationController.java
@@ -3,24 +3,33 @@ package com.scoinone.user.controller;
 import com.scoinone.user.dto.common.DeleteResponseDto;
 import com.scoinone.user.dto.request.notification.CreateNotificationRequestDto;
 import com.scoinone.user.dto.response.notification.CreateNotificationResponseDto;
+import com.scoinone.user.dto.response.notification.GetNotificationsResponseDto;
 import com.scoinone.user.entity.NotificationEntity;
 import com.scoinone.user.mapper.NotificationMapper;
 import com.scoinone.user.service.NotificationService;
+import com.scoinone.user.sse.SseEmitters;
+import java.io.IOException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequestMapping("/api/notification")
 @RequiredArgsConstructor
 public class NotificationController {
     private final NotificationService notificationService;
+    private final SseEmitters sseEmitters;
 
     @PostMapping
     public ResponseEntity<CreateNotificationResponseDto> createNotification(
@@ -30,10 +39,40 @@ public class NotificationController {
                 requestDto.getEmail(),
                 requestDto.getMessage()
         );
+        sseEmitters.notification(
+                notification.getUser().getId(),
+                NotificationMapper.INSTANCE.notificationToGetNotificationResponseDto(notification)
+        );
         return new ResponseEntity<>(
                 NotificationMapper.INSTANCE.notificationToCreateNotificationResponseDto(notification),
                 HttpStatus.CREATED
         );
+    }
+
+    @GetMapping
+    public ResponseEntity<GetNotificationsResponseDto> getNotifications(
+            @RequestHeader(value = "UserId") String userId
+    ) {
+        List<NotificationEntity> notificationsFromLast30Days =
+                notificationService.getNotificationsFromLast30DaysByUserId(userId);
+        return new ResponseEntity<>(
+                NotificationMapper.INSTANCE.listToGetNotificationsResponseDto(notificationsFromLast30Days),
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<SseEmitter> getNotificationStream(@RequestHeader(value = "UserId") String userId) {
+        SseEmitter emitter = new SseEmitter();
+        sseEmitters.add(userId, emitter);
+        try {
+            emitter.send(SseEmitter.event()
+                    .name("ping")
+                    .data("ping"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return ResponseEntity.ok(emitter);
     }
 
     @DeleteMapping("/{notificationId}")
