1b21c541321f9f508173494a971301fa0ecd5589
reindeer002 <lsj000424@gmail.com>
2025-01-20 03:11:27 +0900

    test: add two apis test
    - add get notifications from last 30 day api test
    - add sse api to get notification live stream api test

--- backend/core/user/src/test/java/com/scoinone/user/integration/controller/NotificationControllerTest.java
+++ backend/core/user/src/test/java/com/scoinone/user/integration/controller/NotificationControllerTest.java
@@ -7,6 +7,7 @@ import com.scoinone.user.config.UserDataInitializer;
 import com.scoinone.user.dto.common.DeleteResponseDto;
 import com.scoinone.user.dto.request.notification.CreateNotificationRequestDto;
 import com.scoinone.user.dto.response.notification.CreateNotificationResponseDto;
+import com.scoinone.user.dto.response.notification.GetNotificationsResponseDto;
 import com.scoinone.user.entity.NotificationEntity;
 import com.scoinone.user.entity.UserEntity;
 import com.scoinone.user.repository.NotificationRepository;
@@ -16,10 +17,15 @@ import java.util.Objects;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -27,13 +33,26 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.test.StepVerifier;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import({TestContainerConfig.class, UserDataInitializer.class})
 @ActiveProfiles("dev")
+@AutoConfigureWebTestClient
+@TestMethodOrder(OrderAnnotation.class)
 class NotificationControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
     @Autowired
     private TestRestTemplate restTemplate;
+
+    @Autowired
+    private NotificationService notificationService;
 
     private NotificationEntity savedNotification;
     private UserEntity savedUser;
@@ -57,6 +76,7 @@ class NotificationControllerTest {
 
     @Test
     @DisplayName("알림 생성 테스트")
+    @Order(1)
     void createNotification_shouldReturnCreatedNotification() {
         CreateNotificationRequestDto requestDto = new CreateNotificationRequestDto();
         requestDto.setEmail("user@example.com");
@@ -78,6 +98,26 @@ class NotificationControllerTest {
     }
 
     @Test
+    @DisplayName("사용자 알림 조회 테스트")
+    void getNotifications_shouldReturnListOfNotifications() {
+        notificationService.createNotification(savedUser.getEmail(), "Test Notification1");
+        notificationService.createNotification(savedUser.getEmail(), "Test Notification2");
+        notificationService.createNotification(savedUser.getEmail(), "Test Notification3");
+
+        ResponseEntity<GetNotificationsResponseDto> response = restTemplate.exchange(
+                "/api/notification",
+                HttpMethod.GET,
+                new HttpEntity<>(headers),
+                GetNotificationsResponseDto.class
+        );
+
+        assertSoftly(softly -> {
+            softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+            softly.assertThat(Objects.requireNonNull(response.getBody()).getNotifications()).hasSize(4);
+        });
+    }
+
+    @Test
     @DisplayName("알림 삭제 테스트")
     void deleteNotification_shouldReturnOk() {
         ResponseEntity<DeleteResponseDto> response = restTemplate.exchange(
@@ -93,5 +133,30 @@ class NotificationControllerTest {
             softly.assertThat(Objects.requireNonNull(response.getBody()).getMessage())
                     .contains("Notification deleted successfully");
         });
+    }
+
+    @Test
+    @DisplayName("알림 실시간 조회 테스트 - SSE")
+    void testSseNotificationStream() throws Exception {
+        WebTestClient.ResponseSpec responseSpec = webTestClient.get()
+                .uri("http://localhost:" + port + "/api/notification/stream")
+                .headers(headers -> headers.set("UserId", savedUser.getId()))
+                .exchange();
+
+        CreateNotificationRequestDto requestDto = new CreateNotificationRequestDto();
+        requestDto.setEmail("user@example.com");
+        requestDto.setMessage("This is a test notification.");
+
+        webTestClient.post()
+                .uri("http://localhost:" + port + "/api/notification")
+                .headers(headers -> headers.set("UserId", savedUser.getId()))
+                .bodyValue(requestDto)
+                .exchange();
+
+        StepVerifier.create(responseSpec.returnResult(String.class).getResponseBody())
+                .expectNextMatches(data -> data.contains("ping"))
+                .expectNextMatches(data -> data.contains("notification"))
+                .thenCancel()
+                .verify();
     }
 }
