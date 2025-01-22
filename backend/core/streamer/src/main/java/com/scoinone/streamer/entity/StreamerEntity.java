f4a36d71a258abd5df3285f580ce407262a9e5d0
reindeer002 <lsj000424@gmail.com>
2025-01-22 18:13:27 +0900

    feat: add streamer entity for scraping

---
+++ backend/core/streamer/src/main/java/com/scoinone/streamer/entity/StreamerEntity.java
@@ -0,0 +1,44 @@
+package com.scoinone.streamer.entity;
+
+import com.scoinone.streamer.entity.base.UpdatableEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "streamers")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@Builder
+public class StreamerEntity extends UpdatableEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "virtual_asset_id", nullable = false)
+    private VirtualAssetEntity virtualAsset;
+
+    private String liveStreamingPlatform;
+
+    private String liveStreamingPlatformStreamerId;
+
+    private String searchKeyword;
+
+    private String youtubeId;
+
+    private String naverCafeId;
+}
