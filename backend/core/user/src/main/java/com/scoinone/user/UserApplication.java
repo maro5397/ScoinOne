f735fb11ff101db93dd6718973f6ede79f2e2c41
reindeer002 <lsj000424@gmail.com>
2025-01-22 18:47:03 +0900

    feat: add enable feign clients annotation for using feign clients

--- backend/core/user/src/main/java/com/scoinone/user/UserApplication.java
+++ backend/core/user/src/main/java/com/scoinone/user/UserApplication.java
@@ -2,12 +2,14 @@ package com.scoinone.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableJpaAuditing
+@EnableFeignClients
 public class UserApplication {
 
 	public static void main(String[] args) {
