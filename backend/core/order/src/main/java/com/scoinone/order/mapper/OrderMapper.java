ec20bdf047bbbd687e5fd53528dbb1de800d7351
reindeer002 <lsj000424@gmail.com>
2025-01-29 22:49:12 +0900

    feat: add order mapping function (not sellOrder, buyOrder)

--- backend/core/order/src/main/java/com/scoinone/order/mapper/OrderMapper.java
+++ backend/core/order/src/main/java/com/scoinone/order/mapper/OrderMapper.java
@@ -4,8 +4,14 @@ import com.scoinone.order.dto.response.order.CancelBuyOrderResponseDto;
 import com.scoinone.order.dto.response.order.CancelSellOrderResponseDto;
 import com.scoinone.order.dto.response.order.CreateBuyOrderResponseDto;
 import com.scoinone.order.dto.response.order.CreateSellOrderResponseDto;
+import com.scoinone.order.dto.response.order.GetOrderByUserIdResponseDto;
+import com.scoinone.order.dto.response.order.GetOrderResponseDto;
+import com.scoinone.order.dto.response.order.GetOrdersByUserIdResponseDto;
+import com.scoinone.order.dto.response.order.GetOrdersResponseDto;
 import com.scoinone.order.entity.BuyOrderEntity;
 import com.scoinone.order.entity.SellOrderEntity;
+import com.scoinone.order.entity.base.OrderEntity;
+import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -47,4 +53,47 @@ public interface OrderMapper {
     @Mapping(source = "price", target = "price")
     @Mapping(source = "status.value", target = "status")
     CancelSellOrderResponseDto sellOrderToCancelSellOrderResponseDto(SellOrderEntity sellOrder);
+
+    @Mapping(source = "id", target = "orderId")
+    @Mapping(source = "virtualAssetId", target = "virtualAssetId")
+    @Mapping(source = "quantity", target = "quantity")
+    @Mapping(source = "price", target = "price")
+    @Mapping(source = "createdAt", target = "createdAt")
+    GetOrderResponseDto orderToGetOrderResponseDto(OrderEntity order);
+
+    List<GetOrderResponseDto> orderToGetOrdersResponseDto(List<OrderEntity> orders);
+
+    default GetOrdersResponseDto listToGetOrdersResponseDto(List<OrderEntity> orders) {
+        GetOrdersResponseDto responseDto = new GetOrdersResponseDto();
+        responseDto.setOrders(orderToGetOrdersResponseDto(orders));
+        return responseDto;
+    }
+
+    default GetOrderByUserIdResponseDto orderToGetOrderByUserIdResponseDto(OrderEntity order, String orderId) {
+        GetOrderByUserIdResponseDto dto = new GetOrderByUserIdResponseDto();
+
+        dto.setOrderId(orderId);
+        dto.setVirtualAssetId(order.getVirtualAssetId());
+        dto.setQuantity(order.getQuantity());
+        dto.setPrice(order.getPrice());
+        dto.setCreatedAt(order.getCreatedAt());
+
+        if (order instanceof BuyOrderEntity) {
+            dto.setUserId(((BuyOrderEntity) order).getBuyerId());
+        } else if (order instanceof SellOrderEntity) {
+            dto.setUserId(((SellOrderEntity) order).getSellerId());
+        }
+
+        dto.setStatus(order.getStatus().getValue());
+
+        return dto;
+    }
+
+    List<GetOrderByUserIdResponseDto> orderToGetOrderByUserIdResponseDto(List<OrderEntity> orders);
+
+    default GetOrdersByUserIdResponseDto listToGetOrdersByUserIdResponseDto(List<OrderEntity> orders) {
+        GetOrdersByUserIdResponseDto responseDto = new GetOrdersByUserIdResponseDto();
+        responseDto.setOrders(orderToGetOrderByUserIdResponseDto(orders));
+        return responseDto;
+    }
 }
