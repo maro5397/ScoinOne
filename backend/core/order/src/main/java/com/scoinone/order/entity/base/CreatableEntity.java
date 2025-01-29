53458e998dabf78e2a6c3858700f539806f04a40
reindeer002 <lsj000424@gmail.com>
2025-01-29 19:21:21 +0900

    refactor: move creatable entity

--- backend/core/order/src/main/java/com/scoinone/order/entity/base/CreatableEntity.java
+++ backend/core/order/src/main/java/com/scoinone/order/entity/base/CreatableEntity.java
@@ -4,19 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
-@SuperBuilder
-public abstract class CreatableEntity {
+public class CreatableEntity {
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
