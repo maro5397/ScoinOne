36026465afce21de6bb98bf968e9deded40d1b26
reindeer002 <lsj000424@gmail.com>
2025-01-29 18:33:19 +0900

    refactor: move owned virtual asset controller

---
+++ backend/core/order/src/main/java/com/scoinone/order/controller/OwnedVirtualAssetController.java
@@ -0,0 +1,43 @@
+package com.scoinone.order.controller;
+
+import com.scoinone.order.dto.common.DeleteResponseDto;
+import com.scoinone.order.dto.response.ownedvirtualasset.GetOwnedAssetsResponseDto;
+import com.scoinone.order.entity.OwnedVirtualAssetEntity;
+import com.scoinone.order.mapper.OwnedVirtualAssetMapper;
+import com.scoinone.order.service.OwnedVirtualAssetService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ownedasset")
+@RequiredArgsConstructor
+public class OwnedVirtualAssetController {
+    private final OwnedVirtualAssetService ownedVirtualAssetService;
+
+    @GetMapping
+    public ResponseEntity<GetOwnedAssetsResponseDto> getOwnedVirtualAssets(
+            @RequestHeader(value = "UserId") String userId
+    ) {
+        List<OwnedVirtualAssetEntity> ownedVirtualAssets =
+                ownedVirtualAssetService.getOwnedVirtualAssetsByUserId(userId);
+        return new ResponseEntity<>(
+                OwnedVirtualAssetMapper.INSTANCE.listToGetOwnedAssetsResponseDto(ownedVirtualAssets),
+                HttpStatus.OK
+        );
+    }
+
+    @DeleteMapping
+    public ResponseEntity<DeleteResponseDto> deleteAllCommentByUserId(@RequestParam("userId") String userId) {
+        String result = ownedVirtualAssetService.deleteAllOwnedVirtualAssets(userId);
+        DeleteResponseDto response = new DeleteResponseDto(result);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
