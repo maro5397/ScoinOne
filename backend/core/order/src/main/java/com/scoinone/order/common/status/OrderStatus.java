27fb67ded3493dcab49d43d72c22bfd9a6656fea
reindeer002 <lsj000424@gmail.com>
2025-01-31 21:10:04 +0900

    refactor: delete constructor and add requiredArgsConstructor annotation

--- backend/core/order/src/main/java/com/scoinone/order/common/status/OrderStatus.java
+++ backend/core/order/src/main/java/com/scoinone/order/common/status/OrderStatus.java
@@ -1,16 +1,14 @@
 package com.scoinone.order.common.status;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum OrderStatus {
     PENDING("PENDING"),      // 거래 대기 중 (처리되지 않음)
     COMPLETED("COMPLETED"),    // 거래 완료 (성공적으로 거래됨)
     CANCELED("CANCELED"),;     // 거래 취소됨 (사용자 또는 시스템에 의해)
 
     private final String value;
-
-    OrderStatus(String value) {
-        this.value = value;
-    }
 }
