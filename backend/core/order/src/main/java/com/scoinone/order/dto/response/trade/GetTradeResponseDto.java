bb2743ff765ebe33373309e71bb6a27036ff9935
reindeer002 <lsj000424@gmail.com>
2025-02-01 01:37:33 +0900

    refactor: erase buy/sell id, add createdAt

--- backend/core/order/src/main/java/com/scoinone/order/dto/response/trade/GetTradeResponseDto.java
+++ backend/core/order/src/main/java/com/scoinone/order/dto/response/trade/GetTradeResponseDto.java
@@ -1,6 +1,8 @@
 package com.scoinone.order.dto.response.trade;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,9 +10,9 @@ import lombok.Setter;
 @Setter
 public class GetTradeResponseDto {
     private Long tradeId;
-    private Long buyId;
-    private Long sellId;
     private String virtualAssetId;
     private BigDecimal quantity;
     private BigDecimal price;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
 }
